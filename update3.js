1477a0d4e7aa0b2d9defdf984a65e02abbb61c2d
adbyby <876404903@qq.com>
2018-09-21 22:58:21 +0800

    Updated ruels

--- update3.js
+++ update3.js
@@ -1,6 +1,6 @@
-;??????rl,dir,size,reverse,reverse
+;更新规则url,dir,size,reverse,reverse
 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt	\data\lazy.txt	534435	4	0	
-https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt	2452	4	0	
+https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt	3020	4	0	
 !http://update.adbyby.com/rule3/user.action	\user.action	4	5	0	
 
 
