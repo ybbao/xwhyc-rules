41862b76a710d45f34773b1c1c4e734f876ed192
adbyby <876404903@qq.com>
2019-07-05 22:46:45 +0800

    updated rules

--- update3.js
+++ update3.js
@@ -1,6 +1,6 @@
 ;更新规则url,dir,size,reverse,reverse
 https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/lazy.txt	\data\lazy.txt	544318	4	0	
-https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt	6158	4	0	
+https://raw.githubusercontent.com/adbyby/xwhyc-rules/master/video.txt	\data\video.txt	8155	4	0	
 !http://update.adbyby.com/rule3/user.action	\user.action	4	5	0	
 
 
